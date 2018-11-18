1b50a1ab1626931405ca55f595d42408065e4b22
zhangtianfeng <44530730+zhangtianfengRed@users.noreply.github.com>
2018-11-18 20:30:19 +0800

    代码更新

---
+++ githubGameoffBox4/Assets/script/Map.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+    public int high;
+    public int width;
+    public GameObject ground;
+	// Use this for initialization
+	void Start () {
+        mapStart();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #region 加载关卡
+    public interface load
+    {
+        void loadlevel(int level);
+    }
+
+    class levelload : load
+    {
+
+        public void loadlevel(int level)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(level+1);
+        }
+    }
+    public interface level
+    {
+        void Done();
+    }
+    public class Level:level
+    {
+        public GameObject mylevel { get; set; }
+        private load Load;
+        public int _level { get; set; }
+
+        public Level(load _load)
+        {
+            this.Load = _load;
+        }
+
+        public void Done()
+        {
+            Load.loadlevel(_level);
+        }
+    }
+    #endregion
+
+    void mapStart()
+    {
+        for (int x = 0; x < high; x++) {
+            for(int y = 0; y < width; y++)
+            {
+                Instantiate(ground, new Vector3Int(x, y, 0), Quaternion.identity);
+            }
+        }
+
+    }
+}
---
+++ githubGameoffBox4/Assets/script/playInput.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playInput : MonoBehaviour {
+    Receiver receiver = new Receiver();
+    Commad commadup;
+    Commad commadleft;
+    Commad commadright;
+    Commad commaddown;
+    // Use this for initialization
+    void Start () {
+        
+        commadleft = new Moveleft(receiver);
+        commadright = new MoveRight(receiver);
+        commaddown = new MoveDown(receiver);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            commadup = new Moveup(receiver,);
+            InvokeCommad InvokeCommad = new InvokeCommad(commadup);
+            InvokeCommad.Excutecommad();
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            InvokeCommad InvokeCommad = new InvokeCommad(commadleft);
+            InvokeCommad.Excutecommad();
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            InvokeCommad InvokeCommad = new InvokeCommad(commaddown);
+            InvokeCommad.Excutecommad();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            InvokeCommad InvokeCommad = new InvokeCommad(commadright);
+            InvokeCommad.Excutecommad();
+        }
+    }
+
+    public class InvokeCommad
+    {
+        public Commad Commad;
+        public InvokeCommad(Commad commad)
+        {
+            Commad = commad;
+        }
+        public void Excutecommad()
+        {
+            Commad.Action();
+        }
+    }
+    public class play
+    {
+        public GameObject 
+    }
+
+    public class Receiver
+    {
+        public GameObject mygameobjet { get; set; }
+
+        public void up()
+        {
+            Debug.Log("up");
+        }
+        public void Down()
+        {
+            Debug.Log("down");
+        }
+        public void left()
+        {
+            Debug.Log("left");
+        }
+        public void right()
+        {
+            Debug.Log("right");
+        }
+
+    }
+    public abstract class Commad
+    {
+        protected Receiver PlayRecever;
+        public Commad(Receiver playRecever,GameObject gameObject)
+        {
+            PlayRecever = playRecever;
+            PlayRecever.mygameobjet = gameObject;
+        }
+        public virtual void Action() { }
+    }
+    public class MoveDown : Commad
+    {
+        public MoveDown(Receiver receiver,GameObject gameObject) : base(receiver, gameObject) { }
+        public override void Action()
+        {
+            PlayRecever.Down();
+        }
+    }
+    public class Moveup : Commad
+    {
+        public Moveup(Receiver receiver,GameObject gameObject) : base(receiver, gameObject) { }
+        public override void Action()
+        {
+            PlayRecever.up();
+        }
+    }
+    public class Moveleft : Commad
+    {
+        public Moveleft(Receiver receiver,GameObject gameObject) : base(receiver, gameObject) { }
+        public override void Action()
+        {
+            PlayRecever.left();
+        }
+    }
+    public class MoveRight : Commad
+    {
+        public MoveRight(Receiver receiver,GameObject gameObject) : base(receiver, gameObject) { }
+        public override void Action()
+        {
+            PlayRecever.right();
+        }
+    }
+}
